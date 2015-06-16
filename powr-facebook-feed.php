0a5afa8abac0b8a89d701093cb87c0c694bb4996
powr <powr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-16 17:04:20 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/powr-facebook-feed/trunk@1182029 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powr-facebook-feed.php
+++ powr-facebook-feed.php
@@ -1,14 +1,14 @@
 <?php
     /**
      * @package POWr Facebook Feed
-     * @version 1.4.006
+     * @version 1.4.007
      */
     /*
     Plugin Name: POWr Facebook Feed
     Plugin URI: http://www.powr.io
     Description: Combine and display multiple Facebook pages in a single feed!  Drop the widget anywhere in your theme. Or use the POWr icon in your WP text editor to add to a page or post. Edit on your live page by clicking the settings icon. More plugins and tutorials at POWr.io.
     Author: POWr.io
-    Version: 1.4.006
+    Version: 1.4.007
     Author URI: http://www.powr.io
     */
 
